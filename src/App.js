6dca5bfedb42b23a49525c97b895c2ae506a1a18
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-04-27 14:10:54 +0530

    Button and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
+import "./styles.css"
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increaseHandler() {
+    setCount(count + 1)
+  }
+  function dicreaseHandler() {
+    if (count === 0) {
+      return;
+    }
+    setCount(count - 1)
+  }
+
+
+
   return (
     <div className="App">
-      <h1>this is heading</h1>
-      <h1>this is heading</h1>
+      <h1>{count}</h1>
+      <img onClick={increaseHandler} src="https://cdn0.iconfinder.com/data/icons/ui-16px-perfect-megapack-line/16/82_Add-512.png" alt="increase" />
+      <img onClick={dicreaseHandler} src="https://cdn2.iconfinder.com/data/icons/pinpoint-action/48/remove-256.png" alt="dicrease" />
     </div>
   );
 }
