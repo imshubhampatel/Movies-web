60b7a017bd203bb6fc435f4a81c7291fadb69956
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-04-27 14:32:57 +0530

    learnt Reducer Action

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import "./styles.css"
 
-function App() {
+const ACTIONS = {
+  INCREAMENT: "increament",
+  DICREAMENT: "dicreament"
+}
 
-  const [count, setCount] = useState(0);
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREAMENT:
+      return { count: state.count + 1 }
+    case ACTIONS.DICREAMENT:
+      return { count: state.count - 1 }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, disptach] = useReducer(reducer, { count: 0 })
+
 
   function increaseHandler() {
-    setCount(count + 1)
+    disptach({ type: ACTIONS.INCREAMENT })
   }
+
   function dicreaseHandler() {
-    if (count === 0) {
-      return;
-    }
-    setCount(count - 1)
+    disptach({ type: ACTIONS.DICREAMENT })
+    console.log('Decreasement');
+
   }
-
-
 
   return (
     <div className="App">
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <img onClick={increaseHandler} src="https://cdn0.iconfinder.com/data/icons/ui-16px-perfect-megapack-line/16/82_Add-512.png" alt="increase" />
       <img onClick={dicreaseHandler} src="https://cdn2.iconfinder.com/data/icons/pinpoint-action/48/remove-256.png" alt="dicrease" />
     </div>
