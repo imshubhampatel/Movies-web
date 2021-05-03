6a03b7907a11a9c32bc89431c718a75632eab4a2
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-05-03 18:42:05 +0530

    Feat : Added search container

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,48 @@ import "../styles.css"
 import { setShowFavourite } from "../actions/index"
 
 export default function Navbar(props) {
+  const showResult = true
   const onChangeTab = (val) => {
     console.log(val);
     const { store } = props;
     store.dispatch(setShowFavourite(val))
 
   }
+  const handleChange = (event) => {
+    console.log(event.target.value);
+
+  }
+  const handleSearch = () => { }
+
   return (
     <>
       <nav className="nav">
         <div className="logo"><span>S</span>hu<span>b</span>ha<span>m</span></div>
         <div className="input-search">
-          <input type="text" name="submit" placeholder="Search here" />
-          <input type="submit" name="submit" value="search" />
+          <input type="text" placeholder="Search here" onChange={(event) => handleChange(event)} />
+          <input type="submit" value="search" onClick={handleSearch} />
         </div>
+        {
+          showResult &&
+          <div className="search-container">
+            <div className="movie-card">
+              <div className="action-left">
+                <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2876/862876-v" alt="Movie" />
+              </div>
+              <div className="action-right">
+                <div className="item-top">
+                  <h1>Tara</h1>
+                  <p>lore ispusi this word famid silki a and nothing but a great oppertunity for eceryone</p>
+                </div>
+                <div className="item-bottom">
+                  <h1>Rating :<span>9.4</span></h1>
+                  < button className="btn-addmovies">Add to Movies</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+
         <ul className="items">
           <li><button className="btn btn-movies" onClick={() => onChangeTab(false)}>Movies</button></li>
           <li><button className="btn btn-favourites" onClick={() => onChangeTab(true)}>Favourites</button></li>
